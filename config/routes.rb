d4c4001b7f9d2226cbcf975467ce813638ee9e52
Daniel Stark <a264911@uach.mx>
2016-07-07 15:51:24 -0600

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
